10c62b20734f91b12000f092ce58066fd574aa3c
Gustavo <Hukummi@live.com>
2021-11-09 22:36:50 -0300

    init

---
+++ TP_3/src/menu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "input.h"
+#include "menu.h"
+
+
+int menu(){
+	int selectedOption;
+
+	puts("**** ABM de Empleados ***");
+	puts("1-Alta de empleado");
+	puts("2-Modificar empleado");
+	puts("3-Baja de empleado");
+	puts("4-Informar");
+	puts("5-Salir");
+	puts("\n");
+	getInt("Ingrese una opcion: ", &selectedOption);
+	puts("");
+
+	return selectedOption;
+}
---
+++ TP_3/src/menu.h
@@ -0,0 +1,14 @@
+#ifndef SRC_MENU_H_
+#define SRC_MENU_H_
+
+
+
+#endif /* SRC_MENU_H_ */
+
+
+/**
+ * @brief Displays a menu of options
+ *
+ * @return Selected option
+ */
+int menu();
